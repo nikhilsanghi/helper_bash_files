c9e4fad5e7723f59e6189928310791064e2078d7
nikhilsanghi <nikhilsanghi6@gmail.com>
2021-09-15 06:49:46 +0530

    changed kaggle file

--- install_kaggle.sh
+++ install_kaggle.sh
@@ -2,5 +2,5 @@
 yes | pip install kaggle
 wget --no-check-certificate "https://docs.google.com/uc? export=downloads&id=1SiBlG37kPTR5uV0KKXcaKcaQaoDcVnSA" -O kaggle.json
 mv kaggle.json .kaggle
-chmod 600 ~/.kaggle/kaggle.json
+sudo chmod 600 ~/.kaggle/kaggle.json
 kaggle --version
